6c38aadf28e32ef494cc1321a3bbc718537f232d
David Sveningsson <ext@sidvind.com>
2021-06-05 18:55:05 +0200

    feat: bump node release table (node 10 now returns an error)

--- src/rules/outdated-engines.ts
+++ src/rules/outdated-engines.ts
@@ -11,14 +11,16 @@ interface EOLDescriptor {
 }
 
 const EOL: [string, EOLDescriptor][] = [
-	["0.10.99", { date: "2016-10-31" }],
-	["0.12.99", { date: "2016-12-31" }],
-	["4.99.99", { date: "2018-04-30" }],
-	["5.99.99", { date: "2016-06-30" }],
-	["6.99.99", { date: "2019-04-30" }],
-	["7.99.99", { date: "2017-06-30" }],
-	["8.99.99", { date: "2019-12-31" }],
-	["9.99.99", { date: "2018-06-30" }],
+	["0.10.x", { date: "2016-10-31" }],
+	["0.12.x", { date: "2016-12-31" }],
+	["4.x.x", { date: "2018-04-30" }],
+	["5.x.x", { date: "2016-06-30" }],
+	["6.x.x", { date: "2019-04-30" }],
+	["7.x.x", { date: "2017-06-30" }],
+	["8.x.x", { date: "2019-12-31" }],
+	["9.x.x", { date: "2018-06-30" }],
+	["10.x.x", { date: "2021-04-30" }],
+	["11.x.x", { date: "2019-06-01" }],
 ];
 
 export function* outdatedEngines(pkg: PackageJson): Generator<Message> {
@@ -46,18 +48,20 @@ export function* outdatedEngines(pkg: PackageJson): Generator<Message> {
 	}
 
 	for (const [version, descriptor] of EOL) {
-		const parsed = semver.parse(version);
-		if (semver.satisfies(version, range)) {
-			yield {
-				ruleId,
-				severity,
-				message: `engines.node is satisfied by Node ${
-					parsed.major || `0.${parsed.minor}`
-				} (EOL since ${descriptor.date})`,
-				line: 1,
-				column: 1,
-			};
-			return;
+		const expanded = version.replace(/[xX*]/g, "999");
+		const parsed = semver.parse(expanded);
+		if (!semver.satisfies(expanded, range)) {
+			continue;
 		}
+		const nodeRelease = parsed.major || `0.${parsed.minor}`;
+		const message = `engines.node is satisfied by Node ${nodeRelease} (EOL since ${descriptor.date})`;
+		yield {
+			ruleId,
+			severity,
+			message,
+			line: 1,
+			column: 1,
+		};
+		return;
 	}
 }
