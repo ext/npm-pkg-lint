d812f44df1d57720b41982a6e1056202a70dd741
David Sveningsson <ext@sidvind.com>
2026-03-21 14:00:44 +0100

    fix: allow `link:` in devDependencies

--- src/rules/no-protocol-dependencies.spec.ts
+++ src/rules/no-protocol-dependencies.spec.ts
@@ -32,14 +32,36 @@ it("should report error for file: in dependencies", () => {
 	`);
 });
 
-it("should report error for link: in devDependencies", () => {
+it("should not report error for link: in devDependencies", () => {
 	expect.assertions(1);
 	pkg.devDependencies = { "my-lib": "link:../my-lib" };
+	const { content, ast } = generateAst(pkg);
+	expect(codeframe(content, noProtocolDependencies(pkg, ast))).toMatchInlineSnapshot(`""`);
+});
+
+it("should report error for file: in devDependencies", () => {
+	expect.assertions(1);
+	pkg.devDependencies = { "my-lib": "file:../my-lib" };
+	const { content, ast } = generateAst(pkg);
+	expect(codeframe(content, noProtocolDependencies(pkg, ast))).toMatchInlineSnapshot(`
+		"ERROR: "my-lib" uses the "file:" protocol specifier which is not allowed in published packages (no-protocol-dependencies) at package.json
+		  3 |   "version": "1.2.3",
+		  4 |   "devDependencies": {
+		> 5 |     "my-lib": "file:../my-lib"
+		    |     ^
+		  6 |   }
+		  7 | }"
+	`);
+});
+
+it("should report error for link: in dependencies", () => {
+	expect.assertions(1);
+	pkg.dependencies = { "my-lib": "link:../my-lib" };
 	const { content, ast } = generateAst(pkg);
 	expect(codeframe(content, noProtocolDependencies(pkg, ast))).toMatchInlineSnapshot(`
 		"ERROR: "my-lib" uses the "link:" protocol specifier which is not allowed in published packages (no-protocol-dependencies) at package.json
 		  3 |   "version": "1.2.3",
-		  4 |   "devDependencies": {
+		  4 |   "dependencies": {
 		> 5 |     "my-lib": "link:../my-lib"
 		    |     ^
 		  6 |   }
